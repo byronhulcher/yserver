98fa40df8f92d7aee567f0a8bf297d742c1f6721
byronhulcher <byronhulcher@gmail.com>
2014-08-18 15:18:43 -0400

    Added startSeconds and endSeconds to API

--- app/models/video.js
+++ app/models/video.js
@@ -3,7 +3,9 @@ var Schema       = mongoose.Schema;
 
 var VideoSchema   = new Schema({
   _id: String,
-  youtubeUrl: String
+  youtubeUrl: String,
+  startSeconds: int,
+  endSeconds: int
 });
 
 module.exports = mongoose.model('Video', VideoSchema);
